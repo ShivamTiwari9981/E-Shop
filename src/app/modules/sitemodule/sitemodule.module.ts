5f4fd4c1559411d480c68882addfcb062a179c00
rtf9981@outlook.com <rtf9981@outlook.com>
2023-05-01 22:35:16 +0530

    'Eshop'

---
+++ src/app/modules/sitemodule/sitemodule.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SitemoduleRoutingModule } from './sitemodule-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SitemoduleRoutingModule
+  ]
+})
+export class SitemoduleModule { }
